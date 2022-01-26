4193d602ab0a95415b16c839ef389d4a7d1a7dfe
Michael Bond <bond2798@gmail.com>
2022-01-26 12:36:33 -0600

    finished assignment for running in terminal with node main.js

--- node-calculator/main.js
+++ node-calculator/main.js
@@ -1,20 +1,19 @@
 const readlineSync = require("readline-sync")
 
 function add(num1, num2) {
-    let sum = num1 + num2;
-    return sum
+    return (Number(num1) + Number(num2))
 }
 
 function mul(num1, num2) {
-    console.log(num1 * num2)
+    return (Number(num1) * Number(num2))
 }
 
 function div(num1, num2) {
-    console.log (num1 / num2)
+    return (Number(num1) / Number(num2))
 }
 
 function sub(num1, num2) {
-    console.log(num1 - num2)
+    return (Number(num1) - Number(num2))
 }
 
 var num1 = readlineSync.question("Please enter your first number: ");
@@ -24,13 +23,14 @@ var num2 = readlineSync.question("Please enter your second number: ");
 console.log(num2);
 
 var mathFunction = ["add", "sub", "mul", "div"];
-index = readlineSync.keyInSelect(mathFunction, "Please enter the operation to perform ") 
-    if (mathFunction[1]) {
-        console.log(add(num1, num2))
-    } else if (mathFunction[2]) {
-        console.log(sub(num1, num2))
-    } else if (mathFunction[3]) {
-        console.log(mul(num1, num2))
-    } else if (mathFunction[4]) {
-        console.log(div(num1, num2))
-    }
+index = readlineSync.keyInSelect(mathFunction, "Please enter the operation to perform ");
+console.log(mathFunction[index])
+if (mathFunction[index] == "add") {
+    console.log(add(num1, num2))
+} else if (mathFunction[index] == "sub") {
+    console.log(sub(num1, num2))
+} else if (mathFunction[index] == "mul") {
+    console.log(mul(num1, num2))
+} else if (mathFunction[index] == "div") {
+    console.log(div(num1, num2))
+}

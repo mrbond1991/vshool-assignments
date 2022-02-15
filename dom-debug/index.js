f5f9d2a6312410f4fa8a777e0e03967c4649199e
Michael Bond <bond2798@gmail.com>
2022-02-15 09:04:31 -0600

    innitial commit

--- dom-debug/index.js
+++ dom-debug/index.js
@@ -3,28 +3,30 @@ const colors = ["red", "blue", "green"]
 document.getElementById("add").addEventListener("click", function(e){
     const subItem = createSubItem(e)
     document.getElementById("list").appendChild(subItem)
+    document.getElementById("input").value = ""
 })
 
 function createDropDown(){
     const dropDown = document.createElement("select")
-    for (let i = 0; i < colors; i++){
-        const option = createElement("option") 
+    for (let i = 0; i < colors.length; i++){
+        const option = document.createElement("option") 
         option.innerHTML = colors[i]
         option.value = colors[i]
         dropDown.append(option)
     }
-    dropDown.addEventListener("onchange", function(e){
-        e.target.parent.backgroundColor = e.target.value
+    dropDown.addEventListener("change", function(e){
+        console.log(dropDown.parentElement)
+        dropDown.parentElement.style.backgroundColor = e.target.value
     })
     return dropDown
 }
 
 function createSubItem(e){
     const subItem = document.createElement("div")
-    var subItemValue = document.getElementById("input")
-    subItem.textContent = subItem.value
+    subItem.textContent = document.getElementById("input").value
     const dropDown = createDropDown()
     subItem.appendChild(dropDown)
+    dropDown.parentElement.style.backgroundColor = colors[0]
     subItem.setAttribute("class", "subItem")
     return subItem
 }
---
+++ moms-shopping-list-start/index.js
@@ -0,0 +1,58 @@
+const form = document.addItem
+
+form.addEventListener("submit", (event) =>{
+    event.preventDefault();
+    listItem = createListItem()
+    // document.getElementById("list").appendChild(listItem)
+})
+
+function createListItem() {
+    const listItem = document.createElement("li");
+    listItem.id = "listItem"
+    const listItemName = document.createElement("div")
+    const listItemEdit = document.createElement("button")
+    const listItemX = document.createElement("button")
+    listItemName.textContent = form.title.value;
+    listItemName.id = "itemName"
+    listItemEdit.textContent = "Edit"
+    listItemEdit.id = "edit"
+    listItemEdit.addEventListener("click", () => {
+        const saveButton = document.createElement("button")
+        listItem.prepend(saveButton)
+        saveButton.id = "saveButton"
+        saveButton.textContent = "Save"
+        
+        const liInput = document.createElement("input")
+        liInput.innerHTML = listItemName.value
+        listItem.prepend(liInput)
+        document.getElementById("listItem").removeChild(listItemName)
+
+        document.getElementById("listItem").removeChild(listItemEdit)
+
+        saveButton.addEventListener("click", () => {
+            listItem.prepend(listItemEdit)
+            listItemName.textContent = liInput.value
+            listItem.prepend(listItemName)
+
+            document.getElementById("listItem").removeChild(liInput)
+            
+            document.getElementById("listItem").removeChild(saveButton)
+            
+        })
+    })
+
+    
+
+    listItemX.textContent = "X"
+    listItemX.style.marginLeft = "5px"
+    listItemX.id = "xButton"
+    listItemX.addEventListener("click", () => {
+        document.getElementById("list").removeChild(listItem)
+    })
+    list.appendChild(listItem);
+    listItem.appendChild(listItemName)
+    listItem.appendChild(listItemEdit)
+    listItem.appendChild(listItemX)
+    form.title.value = "";
+}
+

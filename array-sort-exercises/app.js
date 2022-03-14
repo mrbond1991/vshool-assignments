3090a67a632112f7772a3bbacfa85abd908b9e63
Michael Bond <bond2798@gmail.com>
2022-03-13 21:28:20 -0500

    innitial commit

---
+++ array-sort-exercises/app.js
@@ -0,0 +1,35 @@
+//1.
+// const arr = [1, 3, 5, 2, 90, 20]
+
+// arr.sort((a, b) => a - b)
+
+// console.log(arr)
+
+//2.
+// arr.sort((a, b) => b - a)
+
+// console.log(arr)
+
+//3.
+// const arr = ["dog", "wolf", "by", "family", "eaten"]
+
+// arr.sort((a, b) => a.length - b.length)
+
+// console.log(arr)
+
+//4.
+// arr.sort((a, b) => a.localeCompare(b))
+
+// console.log(arr)
+
+//5.
+// const byAge = [
+//     { name: "Quiet Samurai", age: 22 },
+//     { name: "Arrogant Ambassador", age: 100 },
+//     { name: "Misunderstood Observer", age: 2 },
+//     { name: "Unlucky Swami", age: 77 }
+// ]
+
+// byAge.sort((a, b) => a.age - b.age)
+
+// console.log(byAge)

5bc9329158353c77bb446c1e2f4f6f43b88d1350
Michael Bond <bond2798@gmail.com>
2022-03-14 15:09:50 -0500

    Initial Commit

---
+++ multiple-array-methods-part-1/app.js
@@ -0,0 +1,65 @@
+const peopleArray = [
+    {
+        firstName: "Sarah",
+        lastName: "Palin",
+        age: 47
+    },
+    {
+        firstName: "Frank",
+        lastName: "Zappa",
+        age: 12
+    },
+    {
+        firstName: "Rick",
+        lastName: "Sanchez",
+        age: 78
+    },
+    {
+        firstName: "Morty",
+        lastName: "Smith",
+        age: 29
+    },
+    {
+        firstName: "Kyle",
+        lastName: "Mooney",
+        age: 27
+    },
+    {
+        firstName: "Pasha",
+        lastName: "Datsyuk",
+        age: 13
+    },
+    {
+        firstName: "Lev",
+        lastName: "Tolstoy",
+        age: 82
+    }
+]
+
+const newList = []
+
+//1)
+const olderThan18 = peopleArray.filter(person => {
+    if(person.age > 18) {
+        newList.push(person)
+    }
+})
+
+//2)
+const alphLName = newList.sort((a, b) => {
+    if(a.lastName < b.lastName) {
+        return -1
+    }
+    if(a.lastName > b.lastName) {
+        return 1
+    }
+})
+
+//3)
+const finalArr = []
+
+const liLookAlike = alphLName.forEach(person => {
+    finalArr.push("<li>" + person.firstName + " " + person.lastName + " is " + person.age + "</li>")
+})
+
+console.log(finalArr)

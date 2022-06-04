7c14413021b241826608ca873a74b581133a92c5
Michael Bond <bond2798@gmail.com>
2022-06-03 19:59:06 -0600

    updated state for array and adding li elements to dom

--- name-entry/src/App.js
+++ name-entry/src/App.js
@@ -1,35 +1,48 @@
 import React from "react"
 
+
 export default function App() {
-    const [formData, setFormData] = React.useState({
-        name: "",
-    })
+
+    const [formData, setFormData] = React.useState(
+        {
+            name: ""
+        }
+    )
+
+    const [nameList, setNameList] = React.useState([])
 
     function handleChange(event) {
         setFormData(prevFormData => {
-            const {name, value, type} = event.target
+            const {name, value} = event.target
             return {
-                ...prevFormData,
                 [name]: value
             }
         })
     }
 
+    function handleClick(event) {
+        event.preventDefault()
+        setNameList(prevNameList => [...prevNameList, formData])
+    }
+    
+    const nameListElements = nameList.map((name) => (
+        <li key={name.name}>{name.name}</li>
+    ))
+    
     return (
         <div>
             <form>
+                <label htmlFor="fullName">Full Name: </label>
                 <input
+                    id="fullName"
                     type="text" 
                     name="name"
-                    placeholder="Full Name"
                     onChange={handleChange}
                     value={formData.name}
                 />
-                <h1></h1>
-                <button></button>
-                <ol>
-
-                </ol>
+                <h1>{formData.name}</h1>
+                <button onClick={handleClick}>Submit</button>
+                <ul>{nameListElements}</ul>
             </form>
         </div>
     )

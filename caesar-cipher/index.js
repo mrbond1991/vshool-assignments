7aeae0ab5fdee460c5b090d0f0fe9cd5e4d12819
Michael Bond <bond2798@gmail.com>
2022-07-07 21:08:57 -0600

    updated code to match assignment requirements

--- caesar-cipher/index.js
+++ caesar-cipher/index.js
@@ -1,23 +1,19 @@
 var readline = require('readline-sync');
-
-
-
 var input = readline.question('What phrase would you like to encrypt? ').toLowerCase();
-console.log("Your Phrase is: " + input)
+console.log("Your Phrase Is: " + input)
 
-console.log(alphabetPosition(input))
-
-
+var alphabet = "abcdefghijklmnopqrstuvwxyz".split('');
 
 var shift = parseInt(readline.question('How many letters would you like to shift? '));
 
-let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var alphabetPosition = text => 
+  text.split('').map(x => {
+      let shiftedLetter = alphabet.indexOf(x) + shift
+      alphabet[shiftedLetter]
+      if (shiftedLetter > 25) {
+        shiftedLetter = shiftedLetter - 26
+      }
+      return alphabet[shiftedLetter]
+    });
 
-function alphabetPosition(text) {
-    let result = ""
-    for(var i = 0; i < text.length; i++) {
-        var code = text.toUpperCase().charCodeAt(i)
-        if (code > 64 && code < 91) result += (code - 64) + " ";
-    }
-    return result.slice(0, result.length - 1);
-}
+console.log("Your Encripted Phrase Is: " + alphabetPosition(input).join(''));

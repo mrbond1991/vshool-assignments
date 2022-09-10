83a2b2da92b03b666e7da30d746595fa86746dd8
Michael Bond <bond2798@gmail.com>
2022-09-10 17:04:58 -0600

    updated code

--- css-gradient-generator-tool/src/App.js
+++ css-gradient-generator-tool/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Display from './Display';
 import Form from './Form'
 
 function App() {
@@ -7,7 +6,6 @@ function App() {
     <div className="App">
       <h1>CSS Gradient Code Generator</h1>
       <div className='gradient-gen'>
-        <Display />
         <Form />
       </div>
     </div>
--- css-gradient-generator-tool/src/Form.js
+++ css-gradient-generator-tool/src/Form.js
@@ -1,20 +1,70 @@
 import React from 'react'
 
 function Form() {
+    const [formData, setFormData] = React.useState({
+        color1: "#f43434",
+        color2: "#1f90d6",
+        angle: "50"
+    })
+
+    function handleChange(event) {
+        setFormData(prevFormData => {
+            const {name, value} = event.target
+            return {
+                ...prevFormData,
+                [name]: value
+            }
+        })
+    }
+
     return (
-        <form className='gradient-form'>
-            <h2>Options</h2>
-            <div className='form-color-1'>
-                <h4 className='color-1-title'>Color 1</h4>
-                <h4 className='color-1-value'></h4>
-                <input type='color'></input>
+        <div className='gradient-app'>
+            <div className='gradient-display-box' >
+                <div className='gradient-display' style={{background: `linear-gradient(${formData.angle}deg, ${formData.color1}, ${formData.color2})`}} ></div>
+                <textarea 
+                    className='gradient-display-text'
+                    onChange={handleChange}
+                    readOnly value={[
+                        "background: linear-gradient(" + formData.angle + "deg", formData.color1, formData.color2 + ")" + "\r\n" + "-moz-background: linear-gradient(" + formData.angle + "deg", formData.color1, formData.color2 + ")" + "\r\n" + "-webkit: linear-gradient(" + formData.angle + "deg", formData.color1, formData.color2 + ")"
+                    ]}
+                />
             </div>
-            <div className='form-color-2'>
-                <h4 className='color-2-title'>Color 2</h4>
-                <h4 className='color-2-value'></h4>
-                <input type='color'></input>
-            </div>
-        </form>
+            <form className='gradient-form'>
+                <h2 className='gradient-form-title'>Options</h2>
+                <div className='form-color-1'>
+                    <h4 className='color-1-title'>Color 1</h4>
+                    <h4 className='color-1-value'>{formData.color1}</h4>
+                    <input 
+                        type='color'
+                        onChange={handleChange}
+                        name="color1"
+                        className="input-color"
+                        value={formData.color1}
+                    />
+                </div>
+                <div className='form-color-2'>
+                    <h4 className='color-2-title'>Color 2</h4>
+                    <h4 className='color-2-value'>{formData.color2}</h4>
+                    <input 
+                        type='color'
+                        onChange={handleChange}
+                        name="color2"
+                        className="input-color"
+                        value={formData.color2}
+                    />
+                </div>
+                <div className='form-angle'>
+                    <h4>Angle</h4>
+                    <input
+                        type='number'
+                        onChange={handleChange}
+                        name='angle'
+                        className='angle'
+                        value={formData.angle}
+                    />
+                </div>
+            </form>
+        </div>
     )
 }
 
